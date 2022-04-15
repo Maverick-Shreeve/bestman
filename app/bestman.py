e8a2dddcbd32ea0fad4de1091533161c87812671
Maverick Shreeve <maverickshreeve@gmail.com>
2022-04-14 18:54:57 -0700

    Changed API keys

--- app/bestman.py
+++ app/bestman.py
@@ -28,7 +28,7 @@ def validate_length(prompt: str) -> bool:
 
 def generate_keywords(prompt: str) -> List[str]:
     # Load your API key from an environment variable or secret management service
-    openai.api_key = ("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")
+    openai.api_key = ("OPENAI_API_KEY")
     enriched_prompt = f"Generate related branding keywords for {prompt}: "
     print(enriched_prompt)
 
@@ -51,7 +51,7 @@ def generate_keywords(prompt: str) -> List[str]:
 
 def generate_branding_snippet(prompt: str) -> str:
     # Load your API key from an environment variable or secret management service
-    openai.api_key = ("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")
+    openai.api_key = ("OPENAI_API_KEY")
     enriched_prompt = f"Generate upbeat branding snippet for {prompt}: "
     print(enriched_prompt)
 
