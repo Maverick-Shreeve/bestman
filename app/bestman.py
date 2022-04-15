1d98fdca4a1b121b97943a7a3caee21dfd13d8ec
Maverick Shreeve <maverickshreeve@gmail.com>
2022-04-15 13:36:29 -0700

    fixing some API errors

--- app/bestman.py
+++ app/bestman.py
@@ -6,14 +6,15 @@ import re
 
 MAX_INPUT_LENGTH = 32
 
+#to set my API key as an envrionment variable i had to type setx OPENAI_API_KEY "mysecretkey" into terminal
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--input", "-i", type=str, required=True)
+    parser = argparse.ArgumentParser()   #argparse lets me test the API by typing - python bestman.py -i "any input"
+    parser.add_argument("--input", "-i", type=str, required=True)  #also make sure to change directory to app for testing// cd app
     args = parser.parse_args()
     user_input = args.input
 
-    print(f"User input: {user_input}")
+    print(f"User input: {user_input}") #trying out f strings, this line prints out what i type in
     if validate_length(user_input):
         generate_branding_snippet(user_input)
         generate_keywords(user_input)
@@ -27,21 +28,20 @@ def validate_length(prompt: str) -> bool:
 
 
 def generate_keywords(prompt: str) -> List[str]:
-    # Load your API key from an environment variable or secret management service
-    openai.api_key = ("OPENAI_API_KEY")
+    # Load API key from an environment variable 
+    openai.api_key = os.getenv("OPENAI_API_KEY")
     enriched_prompt = f"Generate related branding keywords for {prompt}: "
     print(enriched_prompt)
 
     response = openai.Completion.create(
-        engine="text-davinci-002", prompt=enriched_prompt, max_tokens=32
-    )
+        engine="text-davinci-002", prompt=enriched_prompt, max_tokens=32) # the davinci text is from the openAI website
 
     # Extract output text.
-    keywords_text: str = response["choices"][0]["text"]  # had to do index 0 because thats what the ooutput is alwayas at
+    keywords_text: str = response["choices"][0]["text"]  # without this the API prints out a lot of extra things we dont need
 
     # Strip whitespace.
     keywords_text = keywords_text.strip()
-    keywords_array = re.split(",|\n|;|-", keywords_text)  # imported from re splits strings
+    keywords_array = re.split(",|\n|;|-", keywords_text)  # imported from re splits strings, have to add all the characters (,|\n|;|-)because the way the keywords are printed out
     keywords_array = [k.lower().strip() for k in keywords_array]
     keywords_array = [k for k in keywords_array if len(k) > 0]
 
@@ -49,15 +49,14 @@ def generate_keywords(prompt: str) -> List[str]:
     return keywords_array
 
 
-def generate_branding_snippet(prompt: str) -> str:
+def generate_branding_snippet(prompt: str) -> str:   # reuse a lot of the code from above from the keywords function^^
     # Load your API key from an environment variable or secret management service
-    openai.api_key = ("OPENAI_API_KEY")
+    openai.api_key = os.getenv("OPENAI_API_KEY")
     enriched_prompt = f"Generate upbeat branding snippet for {prompt}: "
     print(enriched_prompt)
 
     response = openai.Completion.create(
-        engine="text-davinci-002", prompt=enriched_prompt, max_tokens=32
-    )
+        engine="text-davinci-002", prompt=enriched_prompt, max_tokens=32)
 
     # Extract output text.
     branding_text: str = response["choices"][0]["text"]
@@ -65,10 +64,10 @@ def generate_branding_snippet(prompt: str) -> str:
     # Strip whitespace.
     branding_text = branding_text.strip()
 
-    # Add ... to truncated statements.
+    # Add ... to the end of the statement.
     last_char = branding_text[-1]
     if last_char not in {".", "!", "?"}:
-        branding_text += "..."
+        branding_text += "..."   # the API can go past the linit so if it does it will end with ...
 
     print(f"Snippet: {branding_text}")
     return branding_text
