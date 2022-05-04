adfb516cbc7d355dc0ec173afc30f174e19c1708
Maverick Shreeve <maverickshreeve@gmail.com>
2022-05-03 18:02:47 -0700

    uploaded to a aws lambda function

--- bestman-infra/lib/bestman-infra-stack.ts
+++ bestman-infra/lib/bestman-infra-stack.ts
@@ -6,10 +6,16 @@ export class BestmanInfraStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const layer = new lambda.LayerVersion(this, "BaseLayer", {
+      code: lambda.Code.fromAsset("lambda_base_layer/layer.zip"),
+      compatibleRuntimes: [lambda.Runtime.PYTHON_3_9],
+    });
+
     const apiLambda = new lambda.Function(this, "ApiFunction" , {
       runtime: lambda.Runtime.PYTHON_3_9,
       code: lambda.Code.fromAsset("../app/"),
       handler: "bestman_api.handler",
+      layers: [layer],
 
      });
   }
